9d7f15ec34fb2ea8b7e0bea27ef34e22ed97ad37
Deuk_Hoi <dh97k@naver.com>
2020-08-12 01:03:41 +0900

    add SlidingUpPanel

--- app/src/main/java/com/deuksoft/deukdrive/MainActivity.kt
+++ app/src/main/java/com/deuksoft/deukdrive/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.deuksoft.deukdrive
 
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
+import com.sothree.slidinguppanel.SlidingUpPanelLayout
 import kotlinx.android.synthetic.main.content_main.*
 import kotlinx.android.synthetic.main.nav_header_main.*
 import retrofit2.Call
@@ -20,28 +23,24 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var sliding_layout : SlidingUpPanelLayout
+    lateinit var newFileList : Array<String>
+    lateinit var adapter : ArrayAdapter<String>
+    lateinit var listView : ListView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
-
+        sliding_layout = findViewById(R.id.SlidingLayout)
+        listView = findViewById(R.id.listView)
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         var toggle = ActionBarDrawerToggle(this, drawerLayout,toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
         navView.setNavigationItemSelectedListener(null)
-
-        val arraylist = ArrayList<String>()
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, arraylist)
-        adapter.add("득")
-        adapter.add("회")
-        adapter.add("김")
-        adapter.notifyDataSetChanged()
-        listView.adapter = adapter;
-
 
         val arraylist2 = ArrayList<String>()
         val adapter2 = ArrayAdapter(this, android.R.layout.simple_list_item_1, arraylist2)
@@ -60,11 +59,6 @@ class MainActivity : AppCompatActivity() {
         adapter2.notifyDataSetChanged()
         Fileload.adapter = adapter2;
 
-
-        test.setOnClickListener{
-            text.setText(test.text.toString())
-            sliding.animateOpen();
-        }
         loadSize()
     }
 
@@ -72,8 +66,10 @@ class MainActivity : AppCompatActivity() {
         val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START)
-        }else if(sliding.isOpened) {
-            sliding.animateClose()
+        }
+        else if(sliding_layout.panelState == SlidingUpPanelLayout.PanelState.EXPANDED)
+        {
+            sliding_layout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED)
         }
     }
 
@@ -84,8 +80,9 @@ class MainActivity : AppCompatActivity() {
                 return true
             }
             R.id.add_icon->{
-                Log.e("dsfdfd","dsfs");
-                sliding.animateOpen()
+                Log.e("dsfdfd","dsfs")
+                AddNewFile()
+                sliding_layout.setPanelState(SlidingUpPanelLayout.PanelState.EXPANDED)
                 return true
             }
         }
@@ -106,6 +103,12 @@ class MainActivity : AppCompatActivity() {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
+    }
+
+    fun AddNewFile(){
+        newFileList = arrayOf("사진 촬영", "스캔", "폴더 만들기", "업로드")
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, newFileList)
+        listView.adapter = adapter
     }
 
     fun loadSize(){
